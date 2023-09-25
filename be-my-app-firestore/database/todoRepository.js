916be8d56234efa7e8a528c082be781a1bc7ecdd
anhth1 <114393245+anhth1@users.noreply.github.com>
2023-09-25 14:29:46 +0700

    add todo

--- be-my-app-firestore/database/todoRepository.js
+++ be-my-app-firestore/database/todoRepository.js
@@ -4,9 +4,12 @@ const todosRef = db.collection('todos');
 export async function getAll() {
     let querySnapshot = await todosRef.get();
     let result = [];
+    //todo: không dùng forEach 
     querySnapshot.forEach(query => {
+        // todo: hàm này viết lại thành hàm prepare data
         result.push({ id: query.id, ...query.data() })
     })
+
     result.sort((a, b) => b.createAt - a.createAt);
 
     return result;
@@ -17,16 +20,21 @@ export async function add(data) {
         ...data,
         createAt: new Date()
     });
+
+    //todo: không getAll 1 lần nữa chỉ trả về  data của todo đã tạo + id 
     const todos = await getAll();
 
     return todos;
 }
 
 export async function updateTodo(ids) {
+
+    //todo: tìm cachs khác không getAll ra rồi fillter , cả delete cũng thế 
     let todos = await getAll();
     const todosToUpdate = todos.filter(todo => ids.includes(todo.id));
     const updationPromises = todosToUpdate.map(todo => todosRef.doc(todo.id).update({ completed: !todo.completed }));
     await Promise.all(updationPromises);// giam tgian
+    //todo : trả về data đã update không trả về tất cả todo, tương tự với delete 
     const newTodos = await getAll();
 
     return newTodos;
